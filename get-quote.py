ef7e74bab7361e5bbc625e9bf117ceb6f0d0ee4a
ragbarata <71442116+ragbarata@users.noreply.github.com>
2020-09-17 12:32:20 -0300

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -8,9 +8,12 @@ def main():
   f.close()
 
   last = len(quotes) - 1
+  second = len(quotes) - 3
   rnd = random.randint(0, last)
+  rnd_1 = random.randint(0, second)
 
-  print(quotes[rnd])
+  print(quotes[rnd], end='')    # O parâmetro end='' é utilizado para remover a linha em branco entre os dois prints
+  print(quotes[rnd_1])
 
 if __name__== "__main__":
   main()
